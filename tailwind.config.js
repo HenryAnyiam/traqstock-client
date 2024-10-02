117a9da692c866ef2fb412a0aeba066442516bba
HenryAnyiam <anyiamhenry7@gmail.com>
2024-10-02 11:48:13 +0100

    Add basic header

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
       textColor: {
         'base-green': '#317023',
         'hover-green': '#b2a741',
+        'hover-gold': '#dbc76f'
       },
+      backgroundColor: {
+        'base-brown': '#613a12',
+        'hover-gold': '#dbc76f',
+      }
     },
   },
   plugins: [],
